7883347c2ff95c1db994d3724443799d0365cc99
Isaac Enriquez <isaac-e@live.com.mx>
2021-07-20 15:59:17 -0600

    initializing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Layout from "./pages/layout/layout"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import Main from "./pages/landing/landing"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Main}></Route>
+        </Switch>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
